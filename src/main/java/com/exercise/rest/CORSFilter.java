b815c76d6aae84ba67e3e32989302b5ea9f00a12
toddnq <38304237+toddnq@users.noreply.github.com>
2018-07-17 21:48:19 +0700

    CORS

---
+++ src/main/java/com/exercise/rest/CORSFilter.java
@@ -0,0 +1,21 @@
+package com.exercise.rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext cre) {
+        cre.getHeaders().add("Access-Control-Allow-Origin", "*");
+        cre.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cre.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        cre.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        cre.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+
+}
